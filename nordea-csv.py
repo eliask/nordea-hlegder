7c1bee2b40df789f39d4d98eeec94e59633327c6
Elias Kunnas <elias.kunnas@digital-identity.com>
2016-11-21 11:23:16 +0200

    Upgrade script Enterprise-readiness™
    - Add support for English transaction files (English CSV header)
    - Deal with some bizarre corner cases with the
    - Be less lazy about input validation
    - Add some useful rules for input categorization (IMO)
    - Tweak hledger fields to my taste

--- nordea-csv.py
+++ nordea-csv.py
@@ -1,14 +1,52 @@
 #!/usr/bin/env python3
 
+from decimal import Decimal
+import csv
 import fileinput
-import re
+import sys
 
-for line in fileinput.input():
-    items = line.split('\t')
-    if len(items) == 14:
-        if items[0] == 'Kirjauspäivä':
-            # It's the documentation line, skip
-            continue
-        for i in items:
-            print(i.strip().replace(',', '.'), end=",")
-        print()
+KNOWN_FIRST_HEADER_COLUMNS = ('Kirjauspäivä', 'Entry date')
+
+HEADER = '''Entry date|Value date|Payment date|Amount|Beneficiary/Remitter|Account number|BIC|Transaction|Reference number|Originator's reference|Message|Card number|Receipt|'''.split('|')
+assert len(HEADER) == 14, len(HEADER)
+IDX_AMOUNT = HEADER.index('Amount')
+
+writer = csv.writer(sys.stdout)
+header_written = False
+
+reader = csv.reader(
+    fileinput.input(),
+    quoting=csv.QUOTE_NONE,
+    delimiter='\t',
+)
+
+def read_row_col(idx, value):
+    if idx == IDX_AMOUNT:
+        # Validate the amount
+        return Decimal(value.replace(',', '.'))
+
+    elif value.count('"') % 2 == 1:
+        # Mismatched quote marks screw up syntax highlighting for hledger.
+        return '{}"'.format(value.strip())
+
+    else:
+        return value.strip()
+
+for row in reader:
+    if len(row) == 2:
+        # Account number line
+        continue
+
+    elif len(row) == 0:
+        # Empty line since the file has CRLF, LF line endings...
+        continue
+
+    assert len(row) == len(HEADER), row
+
+    if row[0] in KNOWN_FIRST_HEADER_COLUMNS:
+        if not header_written:
+            header_written = True
+            writer.writerow(row)
+        continue
+
+    writer.writerow(read_row_col(i,v) for i,v in enumerate(row))
